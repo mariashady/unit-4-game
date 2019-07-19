965b24383ff87f82ea0ba5ae04f9b5795859588c
Maria Shady <maria.shady@yahoo.com>
2019-07-19 17:40:28 -0400

    generated random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
 
 
     //Array of gem images
-    let gemImages = ["assets/images/gem1.png", "assets/images/gem2.png","assets/images/gem3.png", "assets/images/gem4.png"]
+    let gemImages = ["assets/images/gem1.png", "assets/images/gem2.png", "assets/images/gem3.png", "assets/images/gem4.png"]
 
     //Functions
     // function appendGems(){
@@ -19,43 +19,46 @@ $(document).ready(function () {
     //         console.log (pics);
     //     })
     // }
-    
 
-        //Function 1 assigns random number to each gem
-        // debugger;
-        function gemValues() {
 
-            for (let i = 0; i < gemImages.length; i++) {
-                let image = $("<img>");
-                image.addClass("gem-buttons gem gem-image");
-                image.attr("src", gemImages[i]);
-                image.attr("data-letter", Math.floor(Math.random() * 12) +1);
-                $("#crystals").append(image);
+    //Function 1 assigns random number to each gem
+    // debugger;
+    function gemValues() {
 
-            }
+        for (let i = 0; i < gemImages.length; i++) {
+            let image = $("<img>");
+            image.addClass("gem-buttons gem gem-image");
+            image.attr("src", gemImages[i]);
+            image.attr("data-letter", Math.floor(Math.random() * 12) + 1);
+            $("#crystals").append(image);
+
         }
+    }
 
 
 
 
-        //Function 2 to generate random number
-        function playGame() {
-            let counter = 0;
-            $(".yourGuess").text("Your points: " + counter);
-            
-            let targetNumber = Math.floor(Math.random() * 120-19 + 19);
-            console.log(targetNumber);
-        }
+    //Function 2 to generate random number
+    function playGame() {
+        let counter = 0;
+        $(".yourGuess").text("Your points: " + counter);
+
+        let targetNumber = Math.floor(Math.random() * 120 - 19 + 19);
+
+
 
         //Display it on the broweser
+        $(".numberToGuess").text("Number to guess: " + targetNumber);
+        console.log(targetNumber);
+    }
 
-        //When player clicks a gem
+    //When player clicks a gem
 
-        //Assign random number to gem when clicked
+    //Assign random number to gem when clicked
 
-        //if/else statements when player hits target or does not hit target
-        
-        
+    //if/else statements when player hits target or does not hit target
+
+
     //Call functions
     gemValues();
     playGame();
