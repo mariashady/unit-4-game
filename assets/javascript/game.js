14f7dff7389d80df2c380222f6fa166f49c784f0
Maria Shady <maria.shady@yahoo.com>
2019-07-19 10:00:17 -0400

    pseudo coded map of game in js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,82 @@
+$(document).ready(function () {
+
+    //Global Variables
+
+
+    //Array of gem images
+
+
+    //Functions
+
+        //Function 1 assigns random number to each gem
+
+
+
+
+        //Function 2 to generate random number
+
+        //Display it on the broweser
+
+        //When player clicks a gem
+
+        //Assign random number to gem when clicked
+
+        //if/else statements when player hits target or does not hit target
+        
+        
+    //Call functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
