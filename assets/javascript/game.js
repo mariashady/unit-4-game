5135e8afdb66a7b7685db637f0cf690b71b004b5
Maria Shady <maria.shady@yahoo.com>
2019-07-19 17:23:40 -0400

    added functionality and markup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     //Global Variables
     let wins = 0;
     let losses = 0;
+    $(".winsText").text("Wins: " + wins);
+    $(".lossesText").text("Losses: " + losses);
 
 
 
@@ -10,20 +12,40 @@ $(document).ready(function () {
     let gemImages = ["assets/images/gem1.png", "assets/images/gem2.png","assets/images/gem3.png", "assets/images/gem4.png"]
 
     //Functions
-    function appendGems(){
-        gemImages.forEach(function(pics, index){
-            let gemImg = $("<img>") .attr('src', pics  )
-            $("#crystals").append(gemImg)
-            console.log (pics);
-        })
-    }
+    // function appendGems(){
+    //     gemImages.forEach(function(pics, index){
+    //         let gemImg = $("<img>") .attr('src', pics  )
+    //         $("#crystals").append(gemImg)
+    //         console.log (pics);
+    //     })
+    // }
+    
 
         //Function 1 assigns random number to each gem
+        // debugger;
+        function gemValues() {
+
+            for (let i = 0; i < gemImages.length; i++) {
+                let image = $("<img>");
+                image.addClass("gem-buttons gem gem-image");
+                image.attr("src", gemImages[i]);
+                image.attr("data-letter", Math.floor(Math.random() * 12) +1);
+                $("#crystals").append(image);
+
+            }
+        }
 
 
 
 
         //Function 2 to generate random number
+        function playGame() {
+            let counter = 0;
+            $(".yourGuess").text("Your points: " + counter);
+            
+            let targetNumber = Math.floor(Math.random() * 120-19 + 19);
+            console.log(targetNumber);
+        }
 
         //Display it on the broweser
 
@@ -35,7 +57,8 @@ $(document).ready(function () {
         
         
     //Call functions
-    appendGems()
+    gemValues();
+    playGame();
 
 
 
